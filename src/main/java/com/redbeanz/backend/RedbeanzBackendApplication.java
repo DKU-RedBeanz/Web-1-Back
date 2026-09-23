135329d63c8f87c98ca806c232a66a38cafe64ba
Your Name <you@example.com>
2026-09-23 12:42:09 +0900

    config: Spring 공통 프로젝트 및 MySQL 연결 설정 추가

---
+++ src/main/java/com/redbeanz/backend/RedbeanzBackendApplication.java
@@ -0,0 +1,13 @@
+package com.redbeanz.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedbeanzBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedbeanzBackendApplication.class, args);
+	}
+
+}
